dc49eb6d858495ee9ad516f89113486b6146f3a0
FrInve <francesco1.invernici@mail.polimi.it>
2022-03-22 19:45:43 +0100

    Setup project environment

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TO DO:
+
+## GraphMiningPipeline
+
+- Fix linker
+- Fix NPMI
+- Refactor everything
+- Implement use of ploomber or other pipeline manager
+
+## Packaging wtih setuptools and wheel
+Follow [this](https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/#requirements-for-packaging-and-distributing) guide
+
+- Update requirements.txt
+- Update setup.py
+- Build a wheel
+- Build a package
+- Upload to pypi, check twine (Also check [this](https://pyquestions.com/what-is-setup-py))
+
+## Docs
+- Generate docs with pdoc3
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    readme_text = f.read()
+
+setup(
+    name='agave',
+    version='0.1.0.dev1',
+    description='a graphical abstract verification engine',
+    license="bsd-3-clause",
+    long_description=readme_text,
+    author="Francesco Invernici",
+    url="github.com/FrInve/agave",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Natural Language :: English',
+
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    packages=['agave'],
+    #install_requires=[],
+    #scripts=[]
+)
