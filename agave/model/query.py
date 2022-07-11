7e41d6b2d55e01e48e074042189e0bb53c20c875
FrInve <francesco1.invernici@mail.polimi.it>
2022-07-11 03:11:30 +0200

    Add load and export

--- agave/model/query.py
+++ agave/model/query.py
@@ -2,33 +2,33 @@ from dataclasses import dataclass, field
 from typing import List
 import networkx as nx
 
-@dataclass
-class UnaryPredicate:
-    name: str
-
-@dataclass(eq=True, frozen=True)
-class Entity:
-    name: str = field(compare=True)
-    label: str = field(compare=False)
-    predicates : List[UnaryPredicate] = field(compare=False,default_factory=list)
-
-class Cooccurrence:
-    def __init__(self, entity_1, entity_2):
-        self.first_entity = entity_1 if entity_1.name < entity_2.name else entity_2
-        self.second_entity = entity_2 if entity_2.name > entity_1.name else entity_1
-        self.relation_type = "cooccurrence"
-
-    def get_id(self):
-        return str(self.first_entity.name)+'<->'+str(self.second_entity.name)
-
-@dataclass
-class Chain:
-    relations: List[ Cooccurrence ] = field(default_factory=list)
-    entities: List[Entity] = field(default_factory=list)
-
-@dataclass
-class ContainmentBox:
-    chains: List[Entity] = field(default_factory=list)
+#@dataclass
+#class UnaryPredicate:
+#    name: str
+#
+#@dataclass(eq=True, frozen=True)
+#class Entity:
+#    name: str = field(compare=True)
+#    label: str = field(compare=False)
+#    predicates : List[UnaryPredicate] = field(compare=False,default_factory=list)
+#
+#class Cooccurrence:
+#    def __init__(self, entity_1, entity_2):
+#        self.first_entity = entity_1 if entity_1.name < entity_2.name else entity_2
+#        self.second_entity = entity_2 if entity_2.name > entity_1.name else entity_1
+#        self.relation_type = "cooccurrence"
+#
+#    def get_id(self):
+#        return str(self.first_entity.name)+'<->'+str(self.second_entity.name)
+#
+#@dataclass
+#class Chain:
+#    relations: List[ Cooccurrence ] = field(default_factory=list)
+#    entities: List[Entity] = field(default_factory=list)
+#
+#@dataclass
+#class ContainmentBox:
+#    chains: List[Entity] = field(default_factory=list)
 
 class GraphicalAbstract:
     def __init__(self):
@@ -42,3 +42,15 @@ class GraphicalAbstract:
         self.graph.add_nodes_from(chain)
         self.graph.add_edges_from(zip(chain,chain[1:]))
         self.chains.append(chain)
+    
+    def draw(self):
+        return nx.draw_networkx(self.graph)
+    
+    def export(self):
+        return ['<>'.join(chain) for chain in self.chains]
+    
+    def load(self, chains):
+        for s in chains:
+            l = s.split('<>')
+            print(l)
+            self.add_chain(l)
