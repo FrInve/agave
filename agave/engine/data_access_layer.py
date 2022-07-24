594eb6ee7beb32e1f2a80ac02fcb55a2655ca037
FrInve <francesco1.invernici@mail.polimi.it>
2022-07-24 23:18:58 +0200

    Init type hints

--- agave/engine/data_access_layer.py
+++ agave/engine/data_access_layer.py
@@ -5,15 +5,15 @@ import pandas as pd
 from sqlalchemy import create_engine
 import json
 
-class CooccurrencyGraph:
 
-    def __init__(self, uri, user, password):
+class CooccurrencyGraph:
+    def __init__(self, uri: str, user: str, password: str):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def close(self):
+    def close(self) -> None:
         self.driver.close()
 
-    def test_connection(self):
+    def test_connection(self) -> None:
         with self.driver.session() as session:
             try:
                 result = session.read_transaction(self._test_connection)
@@ -26,26 +26,25 @@ class CooccurrencyGraph:
         result = tx.run("MATCH () RETURN 1 LIMIT 1")
         return result
 
-    def get_shortest_paths(self, head, tail):
+    def get_shortest_paths(self, head: str, tail: str):
         with self.driver.session() as session:
-            result = session.read_transaction(
-                self._get_shortest_paths, head, tail)
+            result = session.read_transaction(self._get_shortest_paths, head, tail)
             return result
 
     @staticmethod
-    def _get_shortest_paths(tx, head, tail):
-#        query = (
-#            "MATCH p=allshortestpaths((a)-[*]-(b))"
-#            " WHERE a.entity=$head AND b.entity=$tail"
-#            " UNWIND p AS path"
-#            " UNWIND relationships(path) AS rela"
-#            " WITH path AS path,"
-#            "      avg(rela.frequency) AS average_relationship_frequency,"
-#            "      collect(rela.name) AS relas"
-#            " WHERE average_relationship_frequency > 1"
-#            " RETURN path, average_relationship_frequency, relas"
-#            " ORDER BY average_relationship_frequency DESC"
-#        )
+    def _get_shortest_paths(tx, head: str, tail: str) -> ShortestPathsResult:
+        #        query = (
+        #            "MATCH p=allshortestpaths((a)-[*]-(b))"
+        #            " WHERE a.entity=$head AND b.entity=$tail"
+        #            " UNWIND p AS path"
+        #            " UNWIND relationships(path) AS rela"
+        #            " WITH path AS path,"
+        #            "      avg(rela.frequency) AS average_relationship_frequency,"
+        #            "      collect(rela.name) AS relas"
+        #            " WHERE average_relationship_frequency > 1"
+        #            " RETURN path, average_relationship_frequency, relas"
+        #            " ORDER BY average_relationship_frequency DESC"
+        #        )
         query = (
             "MATCH p=allshortestpaths((a)-[*]-(b))"
             " WHERE a.name=$head AND b.name=$tail"
@@ -54,25 +53,34 @@ class CooccurrencyGraph:
             " WITH path AS path,"
             "      avg(rela.npmi) AS average_relationship_npmi,"
             "      collect(rela.name) AS relas"
-            #" WHERE average_relationship_npmi > 1"
+            # " WHERE average_relationship_npmi > 1"
             " RETURN path, average_relationship_npmi, relas"
             " ORDER BY average_relationship_npmi DESC"
         )
         result = tx.run(query, head=head, tail=tail)
         out = ShortestPathsResult(result)
         return out
-    
-    def resolve_name(self,_name):
+
+    def resolve_name(self, _name: str):
         with self.driver.session() as session:
-            result = session.read_transaction(
-                self._resolve_name, _name)
+            result = session.read_transaction(self._resolve_name, _name)
             return result
-    
+
     @staticmethod
-    def _resolve_name(tx, _name):
-        query = "MATCH (a) WHERE a.name =~ '.*(?i)%s.*' RETURN a.name, a.umls_id, a.frequency" % _name
+    def _resolve_name(tx, _name: str):
+        query = (
+            "MATCH (a) WHERE a.name =~ '.*(?i)%s.*' RETURN a.name, a.umls_id, a.frequency"
+            % _name
+        )
         result = tx.run(query, name=_name)
-        result = [{"name":record['a.name'],"umls_id":record['a.umls_id'],"frequency":record['a.frequency']} for record in list(result)]
+        result = [
+            {
+                "name": record["a.name"],
+                "umls_id": record["a.umls_id"],
+                "frequency": record["a.frequency"],
+            }
+            for record in list(result)
+        ]
         return result
 
 
@@ -83,17 +91,21 @@ class ShortestPathsResult:
         for record in list(result):
             self.records.append(ShortestPathsRecord(record))
 
+
 class ShortestPathsRecord:
     def __init__(self, record):
-        self.path = record['path']
-        self.avg_npmi = record['average_relationship_npmi']
-        self.relationships = record['relas']
-    
-    def __repr__(self):
+        self.path = record["path"]
+        self.avg_npmi = record["average_relationship_npmi"]
+        self.relationships = record["relas"]
+
+    def __repr__(self) -> str:
         return str(self.path)
-    
-    def get_meta_path_name(self):
-        return str(self.path.start_node['name'])+'-->'+str(self.path.end_node['name'])
+
+    def get_meta_path_name(self) -> str:
+        return (
+            str(self.path.start_node["name"]) + "-->" + str(self.path.end_node["name"])
+        )
+
 
 # -----------------------------------------------------
 class PaperCache:
@@ -101,81 +113,84 @@ class PaperCache:
         self.engine = create_engine(db_string)
         try:
             with self.engine.connect() as con:
-                con.execute("SELECT * FROM bigram_paper LIMIT 1").fetchall()    
+                con.execute("SELECT * FROM bigram_paper LIMIT 1").fetchall()
                 print("Connected to paper cache")
         except Exception:
             print("I can't connect to paper cache")
             raise
 
-#        self.by_entity = pickledb.load(path_entity, False)
-#        self.by_bigram = pickledb.load(path_bigram, False)
-#    
-    def get_papers_by_bigram(self, bigram):
+    #        self.by_entity = pickledb.load(path_entity, False)
+    #        self.by_bigram = pickledb.load(path_bigram, False)
+    #
+    def get_papers_by_bigram(self, bigram: str):
         query = """
         SELECT papers
         FROM bigram_paper
         WHERE bigram = '%s'
-        """ % str(bigram)
+        """ % str(
+            bigram
+        )
         try:
             with self.engine.connect() as con:
                 result = con.execute(query).fetchall()
                 return json.loads(result[0][0])
         except Exception:
-            return
+            return []
 
-#    def get_papers_by_bigram(self, bigram):
-#        return self.by_bigram.get(bigram)
-#    
-    def get_papers_by_bigrams(self, bigrams):
+    #    def get_papers_by_bigram(self, bigram):
+    #        return self.by_bigram.get(bigram)
+    #
+    def get_papers_by_bigrams(self, bigrams: list[str]) -> dict:
         result = {}
         for bigram in bigrams:
             result[bigram] = self.get_papers_by_bigram(bigram)
         return result
 
 
-
 # ------------------------------------------------------
+
 
 class Metadata:
     def __init__(self, db_string, sample=False):
-        self.engine = create_engine(db_string) 
+        self.engine = create_engine(db_string)
         try:
             with self.engine.connect() as con:
-                con.execute("SELECT cord_uid FROM metadata LIMIT 1").fetchall()    
+                con.execute("SELECT cord_uid FROM metadata LIMIT 1").fetchall()
                 print("Connected to metadata table")
         except Exception:
             print("I can't connect to metadata table")
             raise
 
-
-#        if sample:
-#            self.df = pd.read_csv(metadata_csv_path, nrows=10000)
-#        else:
-#            self.df = pd.read_csv(metadata_csv_path)
-#        self.df['publish_time'] = pd.to_datetime(self.df['publish_time'])
-#        self.df = self.df.set_index('cord_uid')
-#
+    #        if sample:
+    #            self.df = pd.read_csv(metadata_csv_path, nrows=10000)
+    #        else:
+    #            self.df = pd.read_csv(metadata_csv_path)
+    #        self.df['publish_time'] = pd.to_datetime(self.df['publish_time'])
+    #        self.df = self.df.set_index('cord_uid')
+    #
     def get_paper(self, cord_uid):
         query = """
         SELECT *
         FROM metadata
         WHERE cord_uid = '%s'
-        """ % str(cord_uid)
+        """ % str(
+            cord_uid
+        )
         try:
-            result = pd.read_sql(query,self.engine)
+            result = pd.read_sql(query, self.engine)
             return result.iloc[0].to_dict()
         except Exception:
             print("Paper not found")
             return
-            
-#    def get_paper(self, cord_uid):
-#        try:
-#            result = self.df.loc[cord_uid].to_dict()
-#            result['cord_uid'] = cord_uid
-#            return result
-#        except KeyError:
-#            return
-#    
+
+    #    def get_paper(self, cord_uid):
+    #        try:
+    #            result = self.df.loc[cord_uid].to_dict()
+    #            result['cord_uid'] = cord_uid
+    #            return result
+    #        except KeyError:
+    #            return
+    #
     def get_papers(self, cord_uids):
         result = [self.get_paper(cord_uid) for cord_uid in cord_uids]
         return [elem for elem in result if elem is not None]
